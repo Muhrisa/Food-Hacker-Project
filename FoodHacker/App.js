d63c2188817b8d74580a7d3b9162398d8947413f
Muhrisa <marisaroldan98@gmail.com>
2020-03-05 14:03:56 -0800

    firebase

--- FoodHacker/App.js
+++ FoodHacker/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import Navigator from "./components/Navigator";
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from "firebase";
 
 class App extends Component {
-  render() {
-    return <Navigator />;
-  }
+ 	componentWillMount(){
+  		var firebaseConfig = {
+    	apiKey: "AIzaSyBvf0SfC3QmJoykCpuQVRk2jDshqn3B3qY",
+    	authDomain: "foodhackerdb.firebaseapp.com",
+    	databaseURL: "https://foodhackerdb.firebaseio.com",
+    	projectId: "foodhackerdb",
+    	storageBucket: "foodhackerdb.appspot.com",
+    	messagingSenderId: "610704435607",
+    	appId: "1:610704435607:web:368119793451f3c8b33cd2",
+    	measurementId: "G-FWPCPMYH6J"
+    	};
+  // Initialize Firebase
+    	firebase.initializeApp(firebaseConfig);
+    	//firebase.analytics();
+  		
+  		firebase.database().ref('users/001').set(
+                {
+                    name: 'Roberto Ramirez 001',
+                    age: 21,
+                    comments: "gey"
+                }
+            )
+    }	
+  	render() {
+    	return <Navigator />;
+  	}
 }
 export default App;
 
