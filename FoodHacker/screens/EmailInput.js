1071666640995a7690c5f10de0648a9c1e3a7f95
Muhrisa <marisaroldan98@gmail.com>
2020-03-05 17:42:23 -0800

    updated email name and password

--- FoodHacker/screens/EmailInput.js
+++ FoodHacker/screens/EmailInput.js
@@ -1,11 +1,58 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Alert, Navigation, KeyboardAvoidingView, ScrollView,} from 'react-native';
 import {Text, Icon, Input, Button, SocialIcon} from 'react-native-elements';
+import {BoxPasswordStrengthDisplay} from 'react-native-password-strength-meter';
+
+import firebase from "firebase";
+
+var firebaseConfig = {
+      apiKey: "AIzaSyBvf0SfC3QmJoykCpuQVRk2jDshqn3B3qY",
+      authDomain: "foodhackerdb.firebaseapp.com",
+      databaseURL: "https://foodhackerdb.firebaseio.com",
+      projectId: "foodhackerdb",
+      storageBucket: "foodhackerdb.appspot.com",
+      messagingSenderId: "610704435607",
+      appId: "1:610704435607:web:368119793451f3c8b33cd2",
+      measurementId: "G-FWPCPMYH6J"
+      };
 
 export class EmailInput extends Component {
   static navigationOptions = {
     headerShown: false,
   };
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email: '',
+      first_name: '',
+      password: ''
+    }
+  }
+  //changeUsername(username){
+    //this.setState(username)
+  //}
+
+  _handlePress() {
+    //this.setState({username:username})
+    //this.setState({email: email})
+     Alert.alert("Username value: "+this.state.first_name);
+     Alert.alert("Email value: "+this.state.email);
+
+     //firebase.initializeApp(firebaseConfig);
+     firebase.database().ref('users').set(
+                {
+                    name: this.state.first_name,
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            )
+     //Add the user to our database here
+
+     this.props.navigation.navigate('PasswordInput');
+  }
+
   render() {
     return (
       <KeyboardAvoidingView
@@ -18,25 +65,58 @@ export class EmailInput extends Component {
           keyboardShouldPersistTaps="handled">
           <View style={styles.headerContainer}>
             <Icon name= "ios-person-add" size={80} type="ionicon" color={'#32a8a8'} />
-            <Text h4 style={{textAlign: 'center'}}>Enter Email:</Text>
+            <Text h4 style={{textAlign: 'center'}}>Register User:</Text>
           </View>
-          <Input
+
+          <Input placeholder=" Name"
+            onChangeText ={(text) => this.setState({first_name:text})}
             leftIcon={
               <Icon
-                name="ios-mail"
+                name="ios-person"
                 type="ionicon"
                 color="rgba(50, 168, 168, 1)"
                 size={25}
               />
             }
-            placeholder=" Email"
+            
             inputContainerStyle={{
               borderWidth: 1,
               borderColor: 'white',
               borderLeftWidth: 0,
               height: 50,
               backgroundColor: 'white',
-              marginBottom: 80,
+              marginBottom: 30,
+              marginTop:15,
+            }}
+            placeholderTextColor="grey"
+            autoCapitalize="none"
+            autoCorrect={false}
+            returnKeyType="next"
+            ref={input => (this.email2Input = input)}
+            onSubmitEditing={() => {
+              this.password2Input.focus();
+            }}
+
+          />
+
+          <Input placeholder= " Email"
+            onChangeText ={(text) => this.setState({email:text})}
+            leftIcon={
+              <Icon
+                name="ios-mail"
+                type="ionicon"
+                color="rgba(50, 168, 168, 1)"
+                size={25}
+                
+              />
+            }
+            inputContainerStyle={{
+              borderWidth: 1,
+              borderColor: 'white',
+              borderLeftWidth: 0,
+              height: 50,
+              backgroundColor: 'white',
+              marginBottom: 30,
             }}
             placeholderTextColor="grey"
             autoCapitalize="none"
@@ -49,9 +129,35 @@ export class EmailInput extends Component {
             }}
 
           />
+
+          <Input placeholder=" Password"   
+            onChangeText ={(text) => this.setState({password:text})}
+            leftIcon={
+              <Icon
+                name="lock"
+                color="rgba(50, 168, 168, 1)"
+                size={25}
+              />
+            }
+            
+            inputContainerStyle={{
+              borderWidth: 1,
+              borderColor: 'white',
+              borderLeftWidth: 0,
+              height: 50,
+              backgroundColor: 'white',
+              marginBottom: 30,
+            }}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            autoCorrect={false}
+            returnKeyType="next"
+            ref={input => (this.email2Input = input)}
+          />
+          <BoxPasswordStrengthDisplay password={this.state.password} />
+          
           <View style={styles.btnWrapper}>
-             <Button
-              title="Continue"
+             <Button title="Continue"
               loading={false}
               loadingProps={{size: 'small', color: 'white'}}
               buttonStyle={{
@@ -59,8 +165,8 @@ export class EmailInput extends Component {
                 borderRadius: 15,
               }}
               titleStyle={{fontWeight: 'bold', fontSize: 23}}
-              containerStyle={{marginVertical: 10, height: 50, width: 300}}
-              onPress={() => this.props.navigation.navigate('PasswordInput')}
+              containerStyle={{marginVertical: 40, height: 50, width: 300}}
+              onPress={() => this._handlePress()}
               underlayColor="transparent"
             />
           </View>
